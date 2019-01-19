7dc5d22a817f8c8f6c2fe5858bed7268927096c0
DeenaMary33 <42939533+DeenaMary33@users.noreply.github.com>
2019-01-19 14:35:58 +0530

    a.c

--- a.c
+++ a.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int num;
+  clrscr();
+  scanf("%d",&num);
+  if(num>0)
+  {
+    printf("Positive");
+  }
+  else if(num<0)
+  {
+    printf("Negative");
+  }
+  else
+  {
+    printf("Zero");
+  }
+  getch();
+}

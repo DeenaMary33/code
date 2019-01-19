27fd32f8cf39a6f2b4b883afb6b49030ce7b9bba
DeenaMary33 <42939533+DeenaMary33@users.noreply.github.com>
2019-01-19 19:53:01 +0530

    q4.c

---
+++ q4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%c",&a);
+    if(a>=65 && a<=90 || a>=97 && a<=122)
+    {
+        printf("Alphabet");
+    }
+    else
+    {
+        printf("No");
+    }
+
+    return 0;
+}

30d0624332f6c9a2d0c2b97c40b1fab8f8b729eb
DeenaMary33 <42939533+DeenaMary33@users.noreply.github.com>
2019-01-20 17:06:56 +0530

    set2c.c

---
+++ set2c.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int num,i,flag=0;
+    scanf("%d",&num);
+    for(i=2;i<num;i++)
+    {
+    if(num%i==0)
+    {
+        flag=1;
+    }
+    }
+    if(flag==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+
+    return 0;
+    }

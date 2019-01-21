b5d393a2f0448d4aa9188bac0505f4a8ab103a75
DeenaMary33 <42939533+DeenaMary33@users.noreply.github.com>
2019-01-21 20:59:55 +0530

    set3b.c

---
+++ set3b.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10],n,i,max;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    max=a[0];
+    for(i=0;i<n;i++)
+    {
+    
+        if(max<a[i])
+        {
+            max=a[i];
+        }
+        
+    }
+    printf("%d,Maximum number",max);
+
+    return 0;
+}

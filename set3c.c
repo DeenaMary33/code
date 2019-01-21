b8d712b2f4e245bc472cbceae02b3b5f498dd3f5
DeenaMary33 <42939533+DeenaMary33@users.noreply.github.com>
2019-01-21 21:19:05 +0530

    set3c.c

---
+++ set3c.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10],n,i,min;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    min=a[0];
+    for(i=0;i<n;i++)
+    {
+    
+        if(min>a[i])
+        {
+            min=a[i];
+        }
+        
+    }
+    printf("%d,Minimum number",min);
+
+    return 0;
+}
+

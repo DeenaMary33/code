3def6a78e230879786d12983865f9fe6bea62cd1
DeenaMary33 <42939533+DeenaMary33@users.noreply.github.com>
2019-01-26 21:22:23 +0530

    set4d.c

---
+++ set4d.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    char ch[100];
+    int i,c=0;
+    scanf("%[^\n]s",ch);
+    for(i=0;ch[i]!='\0';i++)
+    {
+        if(ch[i]=='.')
+        {
+            c++;
+        }
+    }
+    printf("%d",c+1);
+}

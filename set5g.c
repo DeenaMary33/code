739cdaf748a4b51e4a86df2b295e19d03d31f52e
DeenaMary33 <42939533+DeenaMary33@users.noreply.github.com>
2019-01-27 19:58:52 +0530

    set5g.h

---
+++ set5g.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100],n,i,min,max;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    max=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(max<a[i])
+        {
+            max=a[i];
+        }
+    }
+    
+    min=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(min>a[i])
+        {
+            min=a[i];
+        }
+    }
+    printf("%d %d",min,max);
+
+    return 0;
+}

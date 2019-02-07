f67adcc64955da8a539960bd7089949b3a19dd96
DeenaMary33 <42939533+DeenaMary33@users.noreply.github.com>
2019-02-07 19:49:21 +0530

    set6c.c

---
+++ set6c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num,r,t=0;
+    scanf("%d",&num);
+    while(num)
+    {
+        r=num%10;
+        t=t+r;
+        num=num/10;
+    }
+    printf("%d",t);
+
+    return 0;
+}
+

6604128d91ff0e93231598e01585bab5008017b9
DeenaMary33 <42939533+DeenaMary33@users.noreply.github.com>
2019-02-23 18:16:19 +0530

    Create pset5g.c

---
+++ pset5g.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>0 && b>0 && c>0)
+    {
+        if(a+b+c==180)
+        {
+            printf("yes");
+        }
+        else
+        {
+            printf("no");
+        }
+    }
+    else
+    {
+        printf("no");
+    }
+
+    return 0;
+}

37e6cfdf370bcbb9ec7c6db27c7a2b7bac158fa3
DeenaMary33 <42939533+DeenaMary33@users.noreply.github.com>
2019-02-23 08:46:50 +0530

    Create pset6e.c

---
+++ pset6e.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main()
+{
+    char s[200],s1[200];
+    int i,flag,n,n1;
+    scanf("%s %s",s,s1);
+    n=strlen(s);
+    n1=strlen(s1);
+    if(n==n1)
+    {
+    for(i=0;i<n;i++)
+      {
+          if((s[i]>='a' && s[i]<='z') ||(s1[i]>='a' && s1[i]<='z'))
+          {
+          if(s[i]==s1[i])
+          {
+              flag=0;
+          }
+          
+          else
+          {
+              flag=1;
+          }
+          }
+          else
+          {
+              flag=1;
+          }
+          
+      }
+    
+    if(flag==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    }
+    else
+    {
+        printf("no");
+    }
+
+    return 0;
+}

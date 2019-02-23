345d5f9bccf4f32ec1ffe77776dcff9110a2d8c0
DeenaMary33 <42939533+DeenaMary33@users.noreply.github.com>
2019-02-23 10:23:07 +0530

    Create pset6b.c

---
+++ pset6b.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int n,a[10],i,temp,j,k;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    for(i=0;i<n;i++)
+    {  
+       for(j=i+1;j<n;j++)
+       {
+           if(a[i]>a[j])
+           {
+           temp=a[i];
+           a[i]=a[j];
+           a[j]=temp;
+           }
+       }
+    }
+    printf("%d",a[k-1]);
+    return 0;
+}

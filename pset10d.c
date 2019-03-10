7bd4eaa262c80397e680d721a47d3e4e22396988
DeenaMary33 <42939533+DeenaMary33@users.noreply.github.com>
2019-03-10 12:30:33 +0530

    Create pset10d.c

---
+++ pset10d.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int n,c=0,i,j,r,flag=0,a[1000];
+    scanf("%d",&n);
+    i=0;
+    while(n)
+    {
+      r=n%10;
+      a[i]=r;
+      i++;
+      n=n/10;
+      c++;
+    }
+    for(i=0;i<c;i++)
+    {
+        for(j=i+1;j<c;j++)
+        {
+            if(a[i]==a[j])
+            {
+                flag=1;
+            }
+        }
+            
+    }
+    if(flag==1)
+    printf("yes");
+    else
+    printf("no");
+
+    return 0;
+}

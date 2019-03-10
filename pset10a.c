d8fc5bb7425ab597868662cf2ae55db83af15204
DeenaMary33 <42939533+DeenaMary33@users.noreply.github.com>
2019-03-10 18:50:36 +0530

    Create pset10a.c

---
+++ pset10a.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n,a[1000],i,s=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+            if(a[i]<a[i+1])
+            {
+                s=s+a[i+1];
+            }
+            else
+            {
+                s=s+a[i];
+            }
+        
+    }
+    printf("%d",s);
+   return 0;
+}

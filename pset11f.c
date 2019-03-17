50940c5530c163a1b1485231b7d3548403baa84c
DeenaMary33 <42939533+DeenaMary33@users.noreply.github.com>
2019-03-17 02:56:12 -0700

    Create pset11f.c

---
+++ pset11f.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int n,a[100],i,j,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    { 
+      for(j=i+1;j<n;j++)
+      {
+          if(a[i]==a[j])
+          {
+              a[j]='$';
+              
+          }
+      }
+          
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]!='$')
+        {
+        printf("%d ",a[i]);
+        }
+    }
+              
+      
+    return 0;
+}
